85b38c71911db11e76e253b8ba3c486192e1fc03
intelligide <intelligide@hotmail.fr>
2020-01-31 14:27:36 -0500

    add decorators

---
+++ src/Core/Decorator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Core
+{
+    public interface Decorable<ReturnT> {
+        ReturnT Operation();
+    }
+
+    public abstract class Decorator<ReturnT>: Decorable<ReturnT> 
+    {
+        protected Decorable<ReturnT> Decorable;
+
+        Decorator(Decorable<ReturnT> decorable)
+        {
+            Decorable = decorable;
+        }
+    }
+
+    public class Compression: Decorator<string>
+    {
+        string Operation()
+        {
+            return "Compressed:" + Decorable.Operation();
+        }
+    }
+
+    public class Encryption: Decorator<string>
+    {
+        string Operation()
+        {
+            return "Encrypted:" + Decorable.Operation();
+        }
+    }
+}
