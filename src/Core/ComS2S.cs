7f81e64e6a74043832176e9b50eecbdb393d9c13
intelligide <intelligide@hotmail.fr>
2020-02-04 12:15:58 -0500

    Fix opts

--- src/Core/ComS2S.cs
+++ src/Core/ComS2S.cs
@@ -2,12 +2,25 @@ using JobSystem;
 
 public class ComS2S
 {
+    public class Options
+    {
+        public Options()
+        {
+            JobSystemOptions = new JobSystemOptions.Builder().Build();
+        }
+        
+        public JobSystemOptions JobSystemOptions { get; set; }
+    }
+
     ServiceCollection Services = new ServiceCollection();
 
-    public ComS2S()  
+    public ComS2S(Options opts = null)  
     {
+        Services.AddService<Options>((IServiceProvider services) => {
+            return opts != null ? opts : new Options();
+        });
         Services.AddService<JobSystemOptions>((IServiceProvider services) => {
-            return new JobSystemOptions.Builder().Build();
+            return services.GetService<Options>().JobSystemOptions;
         });
         Services.AddService<JobSystem.JobSystem>((IServiceProvider services) => {
             return new JobSystem.JobSystem(services.GetService<JobSystem.JobSystemOptions>());
